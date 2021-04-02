36786e8b07fcffefcbc03760e1ace79ae3a74614
Greg Brewton <gbrewton1980@gmail.com>
2021-04-01 22:02:38 -0600

    Adds styled component to PhotoCard

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,4 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const PhotoCardWrapper = styled.div`
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+    width: clamp(400px 60vw 800px);
+    padding: 12px;
+    border-radius: 12px;
+    overflow: hidden;
+    text-align: center;
+    box-shadow: 2px 2px 3px 3px #333;
+    margin: 10px;
+`;
 
 const PhotoCard = (props) => {
     const {
@@ -7,14 +21,16 @@ const PhotoCard = (props) => {
         explanation,
         title,
         url } = props;
-    return <div className="photocard">
+    return (
+        <PhotoCardWrapper>
         <h2>{title}</h2>
         <h3>{date}</h3>
         <h4>By {copyright}</h4>
-        <div id="img_wrapper">
+        <div>
             <img src={url} alt="useful alt text goes here" />
             <p>{explanation}</p>
         </div>
-    </div>
+        </PhotoCardWrapper>
+    )
 }
 export default PhotoCard
