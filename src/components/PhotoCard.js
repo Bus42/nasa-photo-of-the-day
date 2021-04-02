a46fa3ab914f39448d444cf8681d1999dd122b84
Greg Brewton <gbrewton1980@gmail.com>
2021-04-01 22:25:57 -0600

    Adds styling to all components inside PhotoCard

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -5,13 +5,41 @@ const PhotoCardWrapper = styled.div`
     display: flex;
     flex-flow: column nowrap;
     align-items: center;
-    width: clamp(400px 60vw 800px);
+    min-width: 480px;
+    width: 1080px;
+    max-width: 80wv;
     padding: 12px;
     border-radius: 12px;
     overflow: hidden;
     text-align: center;
     box-shadow: 2px 2px 3px 3px #333;
     margin: 10px;
+`;
+const PhotoCardTitle = styled.h2`
+    font-size: 1.5em;
+    font-weight: 700;
+`;
+const PhotoCardDate = styled.h3`
+    font-size: 1.2em;
+    font-weight: 700;
+    margin: 1em;
+`;
+const PhotoCardCredit = styled.h4`
+    font-weight: 500;
+    margin-bottom: .5em;
+`;
+const PhotoCardImageWrapper = styled.div`
+    display: flex;
+    flex-direction: row wrap;
+    justify-content: center;
+`;
+const PhotoCardImage = styled.img`
+    width: 450px;
+`;
+const PhotoCardExplanation = styled.p`
+    text-align: left;
+    text-indent: 1em;
+    margin-left: 1em;
 `;
 
 const PhotoCard = (props) => {
@@ -23,13 +51,13 @@ const PhotoCard = (props) => {
         url } = props;
     return (
         <PhotoCardWrapper>
-        <h2>{title}</h2>
-        <h3>{date}</h3>
-        <h4>By {copyright}</h4>
-        <div>
-            <img src={url} alt="useful alt text goes here" />
-            <p>{explanation}</p>
-        </div>
+            <PhotoCardTitle>{title}</PhotoCardTitle>
+            <PhotoCardDate>{date}</PhotoCardDate>
+            <PhotoCardCredit>By {copyright}</PhotoCardCredit>
+            <PhotoCardImageWrapper>
+                <PhotoCardImage src={url} alt="useful alt text goes here" />
+                <PhotoCardExplanation>{explanation}</PhotoCardExplanation>
+            </PhotoCardImageWrapper>
         </PhotoCardWrapper>
     )
 }
